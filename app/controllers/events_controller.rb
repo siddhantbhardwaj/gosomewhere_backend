200f70dc6f0fa89a8a3bfed2524998e1635c67dd
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2018-02-26 15:41:52 -0400

    only return live or to be events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   before_action :load_current_user
   
   def index
-    @events = Event.all
+    @events = Event.where("end_at > ?", Time.zone.now)
     render json: @events
   end
   
