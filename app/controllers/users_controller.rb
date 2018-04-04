0dea47e1aab5891ba8d92298447fcd6d7bbc982d
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2018-04-03 22:37:04 -0300

    temporarily added user creation path for facebook users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,17 @@ class UsersController < ApplicationController
     render 'users/show'
   end
 
+  def create_from_facebook
+    @user = User.find_or_initialize_by(user_params)
+    @user.password = User.generate_token
+    if @user.save
+      @auth = @user.save_token
+      render 'users/authenticated'
+    else
+      render_errors(@user)
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/users' => 'users#create'
+  post '/users/oauth' => 'users#create_from_facebook'
   get '/users/current' => 'users#current'
   
   post '/signin' => 'sessions#create'
